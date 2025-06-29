37909b65fc476101f026865fa9fa03de4cc4a191
ahmed12v <ahnurmed16@gmail.com>
2025-06-30 00:49:31 +0300

    home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
+import { NotFoundComponent } from './ErorrInRouting/not-found/not-found.component';
 import { HomeComponent } from './pages/home/home.component';
 import { Routes } from '@angular/router';
 
@@ -11,5 +12,14 @@ export const routes: Routes = [
         path:'home',
         loadComponent:()=>
             import('./pages/home/home.component').then((c)=>c.HomeComponent)
-    }
+    },
+    {
+        path:'**',
+        loadComponent:()=>
+            import('./ErorrInRouting/not-found/not-found.component').then((c)=>c.NotFoundComponent)
+    },
+
+    
+
+
 ];
