41e43a09803361a5a9d93816edd505d645ccd5f4
ahmed12v <ahnurmed16@gmail.com>
2025-07-03 11:53:42 +0300

    addCv

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+   
+  constructor(private _Router:Router){}
+  TOAbout()
+  {
+      this._Router.navigate(['/About'])
+  }
+  OpenMyCv()
+  {
+    window.open('../../../assets/Images/Ahmed Sami Frontend Cv (2).pdf', '_blank');
+  }
 }
